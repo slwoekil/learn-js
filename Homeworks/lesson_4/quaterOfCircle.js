20045e6cbb70ca03758f2b1f57e135567fda9be8
Artem Sherstnev <ayo@MacBook-Pro-Artem.local>
2023-04-27 19:09:57 +0200

    lesson_4 done

---
+++ Homeworks/lesson_4/quaterOfCircle.js
@@ -0,0 +1,21 @@
+
+function quaterOfCircle(degree) {
+    const circle = 360;
+    const quater = degree / circle % 1
+
+    if (quater > 0 && quater < 0.25) {
+        return "I";
+    } else if (quater > 0.25 && quater < 0.5) {
+        return "II";
+    } else if (quater > 0.5 && quater < 0.75) {
+        return "III";
+    } else if (quater > 0.75 && quater < 1) {
+        return "IV";
+    } else if (quater === 0.25) {
+        return "Граница I/II";
+    } else if (quater === 0.5) {
+        return "Граница II/III";
+    } else if (quater === 0.75) {
+        return "Граница III/IV";
+    } else return "Граница I/IV"
+}
