fb5a6057e2e5cbb263359ef2c9d333fb6c9e293c
Artem <art.danilov95@gmail.com>
2023-05-15 18:48:20 +0200

    lesson 7 hw done

---
+++ Homeworks/lesson_7/revert.js
@@ -0,0 +1,7 @@
+function reverseArray(exampleArray) {
+    const preparedArray = [];
+    for (const values of exampleArray) {
+        preparedArray.push(values);
+    }
+    return preparedArray.reverse();
+}
