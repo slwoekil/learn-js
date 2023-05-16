0c00f5b36035e10ef328c13685fad7a1cd547423
Artem <art.danilov95@gmail.com>
2023-05-16 10:13:52 +0200

    Пуш нужной версии

--- Homeworks/lesson_7/revert.js
+++ Homeworks/lesson_7/revert.js
@@ -1,14 +1,7 @@
-// function reverseArray(exampleArray) {
-//     for (const [index, value] of exampleArray.entries()) {
-//         const reverseIndex = exampleArray.length - index - 1;
-//     }
-//     return exampleArray[reverseIndex];
-// }
-
-// console.log(reverseArray([1, 2, 3, 4, 5]))
-
-const arr = [1, 2, 3, 4, 5];
-for (const [index, value] of arr.entries()) {
-  const reverseIndex = arr.length - index - 1;
-  console.log(arr[reverseIndex]);
+function reverseArray(exampleArray) {
+    const preparedArray = [];
+    for (const values of exampleArray) {
+        preparedArray.unshift(values);
+    }
+    return preparedArray;
 }
