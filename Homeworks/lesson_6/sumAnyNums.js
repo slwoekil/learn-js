492751ce1d81da46695784747cb64b1da5dc5c57
Artem <art.danilov95@gmail.com>
2023-05-21 11:39:29 +0200

    Переработана работа с ошибками

--- Homeworks/lesson_6/sumAnyNums.js
+++ Homeworks/lesson_6/sumAnyNums.js
@@ -1,13 +1,19 @@
-function sumDivisibleByThree() {
+function sumDivisibleByThree(sameArray) {
     let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        if (typeof arguments[i] !== 'number') {
-            sum = "Передаваемый аргумент должен быть числом"
-            break;
-         }
-    if (arguments[i] % 3 === 0) {
-        sum += arguments[i];
+    for (const value of sameArray) {
+        if (isFinite(value) !== true) {
+            throw new Error(`Аргумент '${value}' должен быть числом`);
         }
+        
+        if (value % 3 === 0) {
+            sum += value;
+        } 
     }
     return sum;
+}
+
+try {
+    console.log(sumDivisibleByThree([1, 2, 3, 4, 5, 6]));
+} catch(e) {
+    console.log(e.message);
 }
