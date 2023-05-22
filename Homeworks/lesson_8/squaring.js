710cadd918d17d8f71857430acc0aac1a0903044
Artem <art.danilov95@gmail.com>
2023-05-22 10:04:40 +0200

    Оптимизация

--- Homeworks/lesson_8/squaring.js
+++ Homeworks/lesson_8/squaring.js
@@ -1,13 +1,11 @@
 function callback(arrayOfNumbers) {
     for (let value of arrayOfNumbers) {
-        if (Number.isFinite(value) !== true) {
-            throw new Error(`Переданное значение '${value}' является ${typeof value} - невозможно возвести в квадрат`)
+        if (!Number.isFinite(value)) {
+            throw new Error(`Переданное значение '${value}' является ${typeof value} - невозможно возвести в квадрат`);
         }
     }
     
-    const newArrayOfNumbers = arrayOfNumbers.map(value => {
-        return value ** 2;
-    })
+    const newArrayOfNumbers = arrayOfNumbers.map(value => value ** 2);
     console.log(newArrayOfNumbers);
 }
 
