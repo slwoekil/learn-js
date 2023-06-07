4f22e94c64ac00b3f5f714a529467e8158aa4873
Artem <art.danilov95@gmail.com>
2023-06-07 10:04:27 +0200

    Поправил отступы

--- Homeworks/lesson_10/exersizes.js
+++ Homeworks/lesson_10/exersizes.js
@@ -1,13 +1,13 @@
 import data from './data.js';
 import { users, updates } from './usersAndUpdates.js';
+
 const getPositions = (data) => {
   const positions = data.map(({position}) => {
     const [col, line ] = position.split(':').map(Number);
     return {col, line};
   });
-
   return positions;
-}
+};
 
 const groupByColor = (data) => {
   const groupedData = data.reduce((accum, item) => {
@@ -17,17 +17,13 @@ const groupByColor = (data) => {
       accum[color] = [];
     }
     accum[color].push(item);
-
     return accum;
   }, {});
-
   return groupedData
-}
+};
 
 console.log(getPositions(data))
 console.log(groupByColor(data))
-
-
 
 const updateUsers = (users, updates) => {
   return users.map((user, index) => {
@@ -39,32 +35,6 @@ const updateUsers = (users, updates) => {
       salary
     }
   })
-}
+};
 
 console.log(updateUsers(users, updates))
-/* 
-На тестовых данных функция должна вернуть
-[
-  {
-    name: 'Name',
-    salary: 200,
-    surname: 'Surname',
-    role: 'DevOps',
-    department: 'DevOpses'
-  },
-  {
-    name: 'Man',
-    salary: 450,
-    department: 'Marketing',
-    surname: 'Gigachad',
-    role: 'Middle Dev',
-  },
-  {
-    name: 'Guy',
-    salary: 120,
-    department: 'Sales',
-    surname: 'Promotion',
-    role: 'Salesman'
-  }
-]
-*/
